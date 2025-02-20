a6bca3ff23404ae66b6ab3fd53e20923e0e30c06
Matador829 <jbuendia829@gmail.com>
2025-02-19 19:28:20 -0800

    State and Reducers eg 1

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "increase":
+            return {...state, count: state.count + 1};
+        case "decrease":
+            return {...state, count: state.count - 1};
+    }
+}
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {count: 0});
   
 return (
     <View>
       <Button title="Increase" onPress={() => {
-        setCounter(counter + 1);
+        dispatch({type: "increase"});
       }} />
       <Button title="Decrease" onPress={() => {
-        setCounter(counter - 1);
+        dispatch({type: "decrease"});
       }} />
-      <Text style={styles.text}>Current Count: {counter}</Text>
+      <Text style={styles.text}>Current Count: {state.count}</Text>
     </View>
   );
 };
